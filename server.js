36fd1b8aea902466d2d13f52cbd505972f1a2709
PRicoSaint <santosesteban09@gmail.com>
2021-07-24 20:05:30 -0500

    Added dotenv package and hid values in .env file

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const inquirer = require('inquirer');
 // Import and require mysql2
 const mysql = require('mysql2');
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    //  Add MySQL password here
-    password: 'password123',
-    database: 'employee_db'
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
   },
   console.log(`Connected to the employee_db database.`)
 );
